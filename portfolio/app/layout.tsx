ca431d9843d6ceedd96702f85548acf81f272c6e
Askyyy <ryanrosalita@g.cjc.edu.ph>
2026-03-17 12:59:15 +0800

    Update layout.tsx

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Shu Huan - Graphic Designer Portfolio",
+  title: "Lynard - Web & Graphic Designer Portfolio",
   description: "Portfolio of Shu Huan, a graphic designer specializing in merchandise design and branding.",
 };
 
